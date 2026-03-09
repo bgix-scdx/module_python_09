86b6a733809c02620e72b4c38bcef0334da4ce12
Bastien Gix <bgix@Bastiens-MacBook-Air.local>
2026-03-09 18:52:23 +0100

    Fixed field not being used according to the subject

--- ex0/space_station.py
+++ ex0/space_station.py
@@ -1,45 +1,40 @@
-from pydantic import model_validator, BaseModel, ValidationError
+from typing import Optional
+from pydantic import model_validator, BaseModel, ValidationError, Field
 
 
 class SpaceStation(BaseModel):
-    name: str
-    power: float
-    oxygen: float
-    crew: int
+    name: str = Field(min_length=1, max_length=50)
+    power: float = Field(ge=80, le=100)
+    oxygen: float = Field(ge=80, le=100)
+    crew: int = Field(ge=1, le=20)
     ID: str
+    Last_maintenance: int
     Operational: bool = True
+    Notes: Optional[str] = Field(max_length=200)
 
     @model_validator(mode="after")
     def validate_crew(self, value):
-        if self.crew < 20:
-            print("Not enought crew to start the program (minimim: 20)")
-        elif len(self.name) > 50:
-            print("Program name denied (maximum: 50)")
-        elif len(self.name) < 3:
-            print("Program name denied (maximum: 50)")
-        elif self.power < 80:
-            print("Not enought power to start the program (minimim: 80)")
-        elif not self.Operational:
-            print("Program not operational, please check the system.")
-        else:
-            print("==================================")
-            print("Valid Space Program validated !")
-            print("crew:", self.crew)
-            print("Name:", self.name)
-            print("ID:", self.ID)
-            print(f"Energy: {self.power}%")
-            print(f"Oxygen: {self.oxygen}%")
-            print(f"Operational: {self.Operational}")
-            print("\n==================================")
-            return self
+        print("==================================")
+        print("Valid Space Program validated !")
+        print("crew:", self.crew)
+        print("Name:", self.name)
+        print("ID:", self.ID)
+        print(f"Energy: {self.power}%")
+        print(f"Oxygen: {self.oxygen}%")
+        print(f"Operational: {self.Operational}")
+        print(f"Last Maintenance: {self.Last_maintenance}")
+        print(f"Notes: {self.Notes}")
+        print("\n==================================")
         return self
 
 
 if __name__ == "__main__":
     try:
-        SpaceStation(**{'crew': 26, 'name': "H.M.ISS", 'power': 90.0,
-                     'oxygen': 100.0, 'ID': "HMI-SS-001", 'Operational': True})
+        SpaceStation(**{'crew': 20, 'name': "H.M.ISS", 'power': 90.0,
+                     'oxygen': 100.0, 'ID': "HMI-SS-001", 'Operational': True},
+                     Notes="From space to baka !", Last_maintenance=0)
         SpaceStation(**{'crew': "Funny", 'name': "H.M.ISS", 'power': 90.0,
-                     'oxygen': 100.0, 'ID': "HMI-SS-001", 'Operational': True})
+                     'oxygen': 100.0, 'ID': "HMI-SS-001", 'Operational': True},
+                     Notes="This should break.", Last_maintenance=600)
     except ValidationError as e:
         print("Invalid input:", e)
